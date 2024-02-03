83f93c8f8e9438865a270cec27202f9a0ebe0e61
Apurba Khanra <77966841+Apurba-Khanra1999@users.noreply.github.com>
2024-02-03 13:48:40 +0530

    Add files via upload

--- Home.py
+++ Home.py
@@ -1,3 +1,30 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Remove AI",
+    page_icon="🤖",
+    layout="wide",
+)
+
 st.title('Background Remover')
+st.write('')
+st.markdown('<h5>Stuck with photos full of boring backgrounds?  Wish you could transport yourself to anywhere in the world with a single tap?  Then buckle up, because [App Name] is here to magically erase backgrounds and ignite your creativity! </h5>',unsafe_allow_html=True)
+st.markdown('<h5>Features so cool, your phone will do a happy dance: </h5>',unsafe_allow_html=True)
+st.markdown('<li>Upload with ease: ✨ Snap pics, grab them from your gallery, or paste a link - backgrounds beware! </li>',unsafe_allow_html=True)
+st.markdown('<li>Format fiesta: We speak image, fluently understanding all the popular ones (JPG, PNG, etc.) </li>',unsafe_allow_html=True)
+st.markdown('<li>AI smarts, stunning results: Our tech isolates your subject like a boss, leaving you with a flawless cutout - say goodbye to green screens! </li>',unsafe_allow_html=True)
+st.markdown('<li>Transparency in a snap: ✨ Instantly create crystal-clear transparent backgrounds, ready to be layered, collaged, and shared with the world! </li>',unsafe_allow_html=True)
+st.markdown('<li>Background buffet: Choose from a universe of new backgrounds or import your own - the possibilities are endless! </li>',unsafe_allow_html=True)
+st.write('')
+st.write('')
+st.markdown("<h5>But wait, there's more! </li>",unsafe_allow_html=True)
+st.markdown('<li>Easy-peasy interface: ✌️ No editing PhD required! Even your grandma can become a background removal ninja. </li>',unsafe_allow_html=True)
+st.markdown("<li>Always on top: We're constantly evolving and learning, bringing you the latest and greatest background removal magic. ✨ </li>",unsafe_allow_html=True)
+st.markdown('<li>Share the love: Instantly post your masterpieces on your favorite social media platforms - let the world see your creativity shine! ✨ </li>',unsafe_allow_html=True)
+st.write('')
+st.write('')
+st.markdown('<h4>Ready to ditch the boring and embrace the extraordinary? Join the background-free revolution! </h4>',unsafe_allow_html=True)
+st.write('')
+
+st.markdown('Feel free to suggest an edit 📝', unsafe_allow_html=True)
+st.markdown('📩 Contact me - apurbakhanra09@gmail.com', unsafe_allow_html=True)
