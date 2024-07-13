d55dccfb1be9191453229b2934b4121cc0228bb6
Apurba Khanra <77966841+Apurba-Khanra1999@users.noreply.github.com>
2024-07-13 16:04:02 +0530

    Add files via upload

--- Home.py
+++ Home.py
@@ -1,30 +1,74 @@
 import streamlit as st
+import subprocess
+import sys
 
 st.set_page_config(
-    page_title="Remove AI",
-    page_icon="🤖",
+    page_title="Tool Box",
+    page_icon="🧊",
     layout="wide",
 )
+st.logo(icon_image="logo.png", image="logofull.png")
+st.title('Our Products')
+st.write('')
+col1, col2, col3, col4 =st.columns(4)
+with col1:
+    with st.container(border=True):
+        left, right = st.columns([1,4])
+        with left:
+            st.image('background.png', width=30)
+        with right:
+            st.write('Remove background from any images')
+        if st.button("Background Remover"):
+            st.switch_page("pages/Background Remover.py")
+with col2:
+    with st.container(border=True):
+        left, right = st.columns([1, 4])
+        with left:
+            st.image('compressor.png', width=30)
+        with right:
+            st.write('Compress image size using sliders')
+        if st.button("Image Compressor"):
+            st.switch_page("pages/Image Compressor.py")
+with col3:
+    with st.container( border= True):
+        left, right = st.columns([1, 4])
+        with left:
+            st.image('zip.png',width=30)
+        with right:
+            st.write('Zip multiple extension files at once')
+        if st.button("Zip Maker"):
+            st.switch_page("pages/Zip Maker.py")
+with col4:
+    with st.container( border= True):
+        left, right = st.columns([1, 4])
+        with left:
+            st.image('pdf.png',width=30)
+        with right:
+            st.write('Convert any images into PDFs')
+        if st.button("Image to Pdf"):
+            st.switch_page("pages/Image to Pdf.py")
 
-st.title('Background Remover')
-st.write('')
-st.markdown('<h5>Stuck with photos full of boring backgrounds?  Wish you could transport yourself to anywhere in the world with a single tap?  Then buckle up, because [App Name] is here to magically erase backgrounds and ignite your creativity! </h5>',unsafe_allow_html=True)
-st.markdown('<h5>Features so cool, your phone will do a happy dance: </h5>',unsafe_allow_html=True)
-st.markdown('<li>Upload with ease: ✨ Snap pics, grab them from your gallery, or paste a link - backgrounds beware! </li>',unsafe_allow_html=True)
-st.markdown('<li>Format fiesta: We speak image, fluently understanding all the popular ones (JPG, PNG, etc.) </li>',unsafe_allow_html=True)
-st.markdown('<li>AI smarts, stunning results: Our tech isolates your subject like a boss, leaving you with a flawless cutout - say goodbye to green screens! </li>',unsafe_allow_html=True)
-st.markdown('<li>Transparency in a snap: ✨ Instantly create crystal-clear transparent backgrounds, ready to be layered, collaged, and shared with the world! </li>',unsafe_allow_html=True)
-st.markdown('<li>Background buffet: Choose from a universe of new backgrounds or import your own - the possibilities are endless! </li>',unsafe_allow_html=True)
-st.write('')
-st.write('')
-st.markdown("<h5>But wait, there's more! </li>",unsafe_allow_html=True)
-st.markdown('<li>Easy-peasy interface: ✌️ No editing PhD required! Even your grandma can become a background removal ninja. </li>',unsafe_allow_html=True)
-st.markdown("<li>Always on top: We're constantly evolving and learning, bringing you the latest and greatest background removal magic. ✨ </li>",unsafe_allow_html=True)
-st.markdown('<li>Share the love: Instantly post your masterpieces on your favorite social media platforms - let the world see your creativity shine! ✨ </li>',unsafe_allow_html=True)
-st.write('')
-st.write('')
-st.markdown('<h4>Ready to ditch the boring and embrace the extraordinary? Join the background-free revolution! </h4>',unsafe_allow_html=True)
-st.write('')
+col5, col6, col7, col8 =st.columns(4)
+with col5:
+    with st.container( border= True):
+        left, right = st.columns([1, 4])
+        with left:
+            st.image('i2w.png',width=30)
+        with right:
+            st.write('Convert multiple images into WEBP')
+        if st.button("Image to WEBP"):
+            st.switch_page("pages/Image to WEBP.py")
+with col6:
+    with st.container( border= True):
+        left, right = st.columns([1, 4])
+        with left:
+            st.image('i2w.png',width=30)
+        with right:
+            st.write('Convert multiple WEBPs into JPG')
+        if st.button("WEBP to JPG"):
+            st.switch_page("pages/WEBP to JPG.py")
 
+
+st.divider()
 st.markdown('Feel free to suggest an edit 📝', unsafe_allow_html=True)
 st.markdown('📩 Contact me - apurbakhanra09@gmail.com', unsafe_allow_html=True)
